e937fceb10d552bb9d6a06e7ccd630b8e3dceb4f
rehaf <ra0633a@american.edu>
2016-10-25 12:22:09 -0400

    updated music manager
    music manager in - need to explain bits and see what should be added in
    terms of states

---
+++ TellTale_proto/Assets/Scripts/Manager-systems/MusicManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+
+	public AudioSource m;
+	public AudioClip[] backgrounds = new AudioClip[5];
+
+	public FameSystem fame;
+	CurrentScenes sceneState;
+
+
+	public bool notChanged = true;
+
+	void Awake(){
+//			clip.playOnAwake = false;
+		m = GetComponent<AudioSource>();
+		m.playOnAwake = false;
+		sceneState = FindObjectOfType<CurrentScenes>();
+		fame = FindObjectOfType<FameSystem>();
+	}
+
+
+	//to send in a specfic clip if needed 
+	public void PlayAudioClip( AudioClip sentclip){
+
+		m.playOnAwake = false;
+		m.Stop();
+		m.clip =sentclip;
+		m.Play();
+
+	}
+
+
+
+	public void changeMusic(){
+
+
+		m.Stop();
+		Debug.Log("called music chnage");
+		if(sceneState.CurrentS ==  CurrentScenes.SceneStaet.market){
+			Debug.Log("Market music should be plaing");
+		
+			m.clip = backgrounds[0];
+			m.Play();
+			notChanged = false;
+
+		}
+		else if( sceneState.CurrentS == CurrentScenes.SceneStaet.tavern){
+			Debug.Log("Home music  music should be plaing");
+
+			if(FameSystem.TavernState.high == fame.tavernSatte){
+
+
+			m.clip = backgrounds[1];
+			m.Play();
+			notChanged = false;
+				
+				//play music for high or fancy 
+			} else if( FameSystem.TavernState.trash == fame.tavernSatte){
+				
+				m.clip = backgrounds[1];
+				m.Play();
+				notChanged = false;
+				
+			}
+			else {
+				
+			
+				m.clip = backgrounds[1];
+				m.Play();
+				notChanged = false;
+			}
+	}
+
+
+	}
+
+
+
+
+	public void stopMusic(){
+		m.Stop();
+	}
+
+}
+
---
+++ TellTale_proto/Assets/TransformControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformControl : MonoBehaviour {
+
+
+	MusicManager music;
+	CurrentScenes c;
+	public void Awake(){
+
+		music = FindObjectOfType<MusicManager>();
+		c = FindObjectOfType<CurrentScenes>();
+	}
+
+	public void OnEnable(){
+		c.changeSceneState();
+		music.changeMusic();
+
+	}
+
+	public void OnDisable(){
+
+		music.stopMusic();
+		
+	}
+}
