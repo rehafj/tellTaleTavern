5fef3515d7d4e6e909dfd84de39533a85fba45be
rehaf <ra0633a@american.edu>
2016-11-04 01:02:10 -0400

    updates
    updates in my test scene
    created a system that adds them on the fly if they have not been added
    - so can create them dynamically instead of a list ——
    appearing box with text for each achieve
    optiosn for fungus vs object visits

---
+++ TellTale_proto/Assets/Scripts/Manager-systems/AchivSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchivSystem : MonoBehaviour {
+
+	public string titile;
+	public bool unlocked;
+	public string discription;
+
+	public 	AchivSystem(string _tiitle, bool _unlocked, string  _discription){
+
+
+		this.titile = _tiitle;
+		this.unlocked = _unlocked;
+		this.discription =_discription;
+	}
+
+
+}
