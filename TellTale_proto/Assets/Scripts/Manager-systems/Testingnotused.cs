fc4cd822391ec566e313c1c01cf7b57ece4ab1b0
rehaf <ra0633a@american.edu>
2016-11-04 20:03:52 -0400

    test - if effects main scene
    testing on rehafTest

---
+++ TellTale_proto/Assets/Scripts/Manager-systems/Testingnotused.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testingnotused : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}//test
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
