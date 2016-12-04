7f12f5a75f6cf14de43307e24f8859d4351a30f9
rehaf <ra0633a@american.edu>
2016-12-03 19:56:44 -0500

    updates
    still buggy
    need to discuss mov options - placement of gui would help or
    restructure system - other say should work —
    —added achieve - buggy - but logically works ( almost there)
    — last all can stop by time for now?

---
+++ TellTale_proto/Assets/DisplayUnlockedNew.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayUnlockedNew : MonoBehaviour {
+
+	// Use this for initialization
+	DisplayAchiv achivm;
+	void Start () {
+		achivm= 	FindObjectOfType<DisplayAchiv>();
+	
+	}
+	
+	// Update is called once per frame
+	void OnEnable () {
+
+		achivm.DisplayAchivments();
+	}
+
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.B)){
+			
+			achivm.DisplayAchivments();
+		}
+	}
+}
