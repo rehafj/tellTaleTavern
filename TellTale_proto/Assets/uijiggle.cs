ff1b3f1e437e7acb167b2090f5183c8f2bd4d57f
AubreyMHill <aubrey.hill730@gmail.com>
2016-12-07 03:09:35 -0500

    blinking buttons yall

--- TellTale_proto/Assets/uijiggle.cs
+++ TellTale_proto/Assets/uijiggle.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class uijiggle : MonoBehaviour {
-    GameObject button;
-    SpriteRenderer flashingRenderer;
+    public GameObject button;
+    public SpriteRenderer flashingRenderer;
     Color initial;
-    Color flash = new Color (1f,1f,1f,0f);
-
+    Color flash = new Color (1f,1f,1f,.75f);
+    int waitVar;
+    bool pause = true;
     void Start()
-    {
+    {   
         this.flashingRenderer = this.button.GetComponent<SpriteRenderer>();
         this.initial = this.flashingRenderer.color;
         //Call coroutine BlinkText on Start
@@ -20,15 +21,24 @@ public class uijiggle : MonoBehaviour {
     //function to blink the image 
     public IEnumerator BlinkRender()
     {
+    
         //blink it forever. You can set a terminating condition depending upon your requirement
         while (true)
         {
-          
+            if (pause == true)
+            {
+                waitVar = Random.Range(1, 6);
+                for (int i = 1; i < waitVar; i++)
+                {
+
+                }
+                pause = false;
+            }
            this.flashingRenderer.color = flash;
             yield return new WaitForSeconds(.5f);
             //display original color for the next 0.5 seconds
             this.flashingRenderer.color = this.initial;
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(1f);
         }
     }
 
