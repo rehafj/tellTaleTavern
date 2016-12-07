43c7f95d0bad32b7c154ff3273d690c17518e647
AubreyMHill <aubrey.hill730@gmail.com>
2016-12-07 02:31:48 -0500

    Fixing Market UI

--- TellTale_proto/Assets/uijiggle.cs
+++ TellTale_proto/Assets/uijiggle.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class uijiggle : MonoBehaviour {
+    GameObject button;
+    SpriteRenderer flashingRenderer;
+    Color initial;
+    Color flash = new Color (1f,1f,1f,0f);
 
-    GameObject uiobject;
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        this.uiobject.transform.Rotate(0, 0,20); 
-        this.uiobject.transform.Rotate(0, 0, 0);
-        this.uiobject.transform.Rotate(0, 0, -20);
-        this.uiobject.transform.Rotate(0, 0, 0);
+    void Start()
+    {
+        this.flashingRenderer = this.button.GetComponent<SpriteRenderer>();
+        this.initial = this.flashingRenderer.color;
+        //Call coroutine BlinkText on Start
+        StartCoroutine(BlinkRender());
     }
+
+    //function to blink the image 
+    public IEnumerator BlinkRender()
+    {
+        //blink it forever. You can set a terminating condition depending upon your requirement
+        while (true)
+        {
+          
+           this.flashingRenderer.color = flash;
+            yield return new WaitForSeconds(.5f);
+            //display original color for the next 0.5 seconds
+            this.flashingRenderer.color = this.initial;
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
 }
