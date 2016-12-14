d2caf123f2e6bf6ceaa5623aa0e9989df643cdfc
AubreyMHill <aubrey.hill730@gmail.com>
2016-12-14 18:46:47 -0500

    More market tweaks

---
+++ TellTale_proto/Assets/BigSmall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigSmall : MonoBehaviour {
+public Transform originalSize;
+Transform newSize;
+//public GameObject shopItem;
+
+	// Use this for initialization
+	void Start () {
+        originalSize = this.transform;
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   public void Expand()
+    {
+        this.newSize.transform.localScale = (this.originalSize.localScale += new Vector3(0.3F, 0.3F, 0.3F));
+        this.transform.localScale = newSize.localScale;
+    }
+    public void Contract()
+    {
+        this.newSize.transform.localScale =(this.originalSize.localScale);
+        this.transform.localScale = newSize.localScale;
+    }
+} 
